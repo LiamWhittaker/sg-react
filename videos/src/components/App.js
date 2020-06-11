50d5292c89fba353cc7a6f95812dbdd1d2d09eaf
Liam <liamgwhittaker@gmail.com>
2020-06-11 19:46:34 +0100

    Finished section 11 - youtube player app

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 export default class App extends Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('Blind Guardian Live');
+  }
+  
   onTermSubmit = async (term) => {
     const res = await youtube.get('/search', {
       params: {
@@ -12,14 +18,30 @@ export default class App extends Component {
       }
     });
 
-    this.setState({videos: res.data.items});
+    this.setState({
+      videos: res.data.items,
+      selectedVideo: res.data.items[0]
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video});
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I found {this.state.videos.length} videos!
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column"> 
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={ this.state.videos }/>
+            </div>
+          </div>    
+        </div>
       </div>
     );
   }
