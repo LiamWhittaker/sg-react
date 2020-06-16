7dd95b8f7718334b1190a5e704eecaa45f11d6b2
Liam <liamgwhittaker@gmail.com>
2020-06-16 21:16:43 +0100

    half of section 15

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,9 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-  return async function(dispatch, getState) {
+  return async function(dispatch) {
     const res = await jsonPlaceholder.get('/posts');
 
-    dispatch({type: 'FETCH_POSTS', payload: res});
+    dispatch({type: 'FETCH_POSTS', payload: res.data });
   }
 }
