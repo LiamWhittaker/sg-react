10b57bef2ef6a5ec56a56a0edc791cd10a7b3d2a
Liam <liamgwhittaker@gmail.com>
2020-06-16 15:39:56 +0100

    Section 14 - redux and middleware

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions'
+// import axios from 'axios'
+
+export class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        PostList!
+      </div>
+    )
+  }
+}
+
+export default connect(null, {fetchPosts})(PostList)
